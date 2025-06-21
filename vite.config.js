ac570128794120894916ff617dc8b3dd139ff3a5
Azure <azurephy@gmail.com>
2025-06-21 01:59:39 -0500

    🔧 Fix: mark supabase as external for Vercel build

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      "@/": "/src/"
+  build: {
+    rollupOptions: {
+      external: ['@supabase/supabase-js']
     }
   }
 })
